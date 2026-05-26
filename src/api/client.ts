a5bee4423974f0c85952949d9ae59705931abd48
Youssef Masoud <youssefmasoud119@gmail.com>
2026-05-26 21:03:05 +0300

    v1

--- src/api/client.ts
+++ src/api/client.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = import.meta.env.VITE_API_URL || 'https://localhost:60111';
+const BASE_URL = import.meta.env.VITE_API_URL || 'https://zyntracenter.runasp.net';
 
 const apiClient = axios.create({
   baseURL: `${BASE_URL}/api/v1`,
